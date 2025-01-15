c4b9f140c253531eb036b20dd6ce5d89502c149d
wanghairui <wanghairui@itasktour.com>
2025-01-15 10:15:12 +0800

    删除备注

--- AnyTime/AnyTimeMe/AnyTimeMeSetting/AnyTimeMeSettingViewController.m
+++ AnyTime/AnyTimeMe/AnyTimeMeSetting/AnyTimeMeSettingViewController.m
@@ -26,13 +26,10 @@
     // Do any additional setup after loading the view.
     self.hidesBottomBarWhenPushed = YES;
 
-    // 设置导航栏透明
     [AnyNavigationBarUtil setNavigationBarTransparent:self transparent:YES];
     
-    // 设置导航栏标题字体和颜色
     [AnyNavigationBarUtil setNavigationBarTitleFont:self font:[UIFont boldSystemFontOfSize:17] color:[UIColor blackColor]];
     
-    // 自定义返回按钮
     [AnyNavigationBarUtil setCustomBackButton:self image:[UIImage imageNamed:@"back"] action:@selector(backClick)];
     
     self.navigationItem.title = @"Set Up";
